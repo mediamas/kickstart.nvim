348223a442ca6331c290bae62c047d3fed9aa1f3
José Aldana <pepe@mediamas.com>
2024-10-14 19:04:15 -0600

    Cambios varios

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -5,7 +5,15 @@ return {
     dependencies = 'nvim-tree/nvim-web-devicons',
     config = function()
       local bufferline = require 'bufferline'
-      bufferline.setup {}
+      bufferline.setup {
+        options = {
+          numbers = 'none', -- Display buffer index or not
+          diagnostics = 'nvim_lsp', -- Show diagnostics in the tabs
+          offsets = { { filetype = 'NvimTree', text = 'File Explorer', text_align = 'center' } },
+          separator_style = 'slant', -- Choose the separator style for tabs
+          always_show_bufferline = false,
+        },
+      }
     end,
   },
 }
